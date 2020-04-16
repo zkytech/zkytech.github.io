8473a60abec3e47c69d9e40d730cae993adcab96
zkytech <zhangkunyuan@hotmail.com>
2020-04-16 19:03:19 +0800

    auto generated commit message

--- scripts/theme_inject.js
+++ scripts/theme_inject.js
@@ -1,3 +1,4 @@
 hexo.extend.filter.register("theme_inject", function (injects) {
-	injects.comment.file("utteranc-comment", "source/_data/utteranc.swig");
+	// 注入swig https://theme-next.org/docs/advanced-settings#Injects
+	// injects.comment.file("utteranc-comment", "source/_data/utteranc.swig");
 });
